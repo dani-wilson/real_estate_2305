edf7fd91d93bd89e23938a9ea59b4abf341ed0ab
Dani Wilson <danirae.powell@gmail.com>
2023-05-18 09:30:28 -0600

    Add .paint test

--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -26,5 +26,13 @@ RSpec.describe Room do
 
       expect(room.is_painted?).to eq(false)
     end
+
+    it 'can be painted' do
+      room = Room.new(:bedroom, 10, '13')
+
+      room.paint
+
+      expect(room.is_painted?).to eq(true)
+    end
   end
 end
