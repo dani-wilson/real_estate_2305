be99c4aa2f6f0e88dbca853e8e0d6f75a29463fe
Dani Wilson <danirae.powell@gmail.com>
2023-05-18 09:48:40 -0600

    Add 'has rooms' and 'can add new rooms' tests

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -7,4 +7,27 @@ RSpec.describe House do
     it 'exists' do
         expect(house).to be_a House
     end
+
+    it 'should have a price as an integer' do
+        expect(house.price).to eq(400000)
+    end
+
+    it 'should have an address' do
+        expect(house.address).to eq("123 sugar lane")
+    end
+
+    it 'has rooms' do
+        expect(house.rooms).to eq([])
+    end
+
+    it 'can add new rooms' do
+        room_1 = Room.new(:bedroom, 10, '13')
+        room_2 = Room.new(:bedroom, 11, '15')
+
+        house.add_room(room_1)
+        house.add_room(room_2)
+
+        expect(house.rooms.length).to eq(2)
+    end
+
 end
