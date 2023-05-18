b4c3c0f125fa9261af0fed51ef877606537357b8
Dani Wilson <danirae.powell@gmail.com>
2023-05-18 09:34:04 -0600

    Add 'it exists' test

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -2,4 +2,9 @@ require './lib/room'
 require './lib/house'
 
 RSpec.describe House do
+    house = House.new("$400000", "123 sugar lane")
+
+    it 'exists' do
+        expect(house).to be_a House
+    end
 end
