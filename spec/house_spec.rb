eb2e5411505c948f9cdbd702993ef430dc50d38b
Dani Wilson <danirae.powell@gmail.com>
2023-05-18 10:02:53 -0600

    Add test for keeping rooms based on category

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -34,4 +34,19 @@ RSpec.describe House do
         expect(house.above_market_average?).to eq(false)
     end
 
+    it 'keeps track of rooms based on category' do
+        room_1 = Room.new(:bedroom, 10, '13')
+        room_2 = Room.new(:bedroom, 11, '15')
+        room_3 = Room.new(:living_room, 25, '15')
+        room_4 = Room.new(:basement, 30, '41')
+
+        house.add_room(room_1)
+        house.add_room(room_2)
+        house.add_room(room_3)
+        house.add_room(room_4)
+
+        expect(house.rooms_from_category).to eq(2)
+        expect(house.rooms_from_category).to eq(1)
+    end
+
 end
